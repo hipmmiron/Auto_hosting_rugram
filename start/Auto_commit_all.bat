39ca5b6a3e57ead05a0acaddde587c699fb27818
Salomatin_Miron <miron.salomatin@gmail.com>
2026-01-18 17:52:35 +0300

    System: Repository Cleared & Beacon Updated

--- start/Auto_commit_all.bat
+++
@@ -1,6 +0,0 @@
-cd "C:\Users\Miron\Documents\Мои документы\Программирование\Python\Мои программы\Rugramm"
-git rm -r --cached .
-git add .
-git commit -m "Update"
-git push https://github.com/hipmmiron/Rugram.git main --force
-pause
--- start/starter.bat
+++
@@ -1,5 +0,0 @@
-title Rugram link generator
-cls
-@echo off
-chcp 65001 >nul
-python "%~dp0link_generator.py"
